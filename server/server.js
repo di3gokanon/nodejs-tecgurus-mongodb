7bd7b18a8630f658d107ac6878ce93f4b79c4d33
Diego <diego.paniagua.kanon89@gmail.com>
2017-02-20 14:33:18 -0600

    Agregando peticion GET /todos

--- server/server.js
+++ server/server.js
@@ -22,6 +22,14 @@ app.post('/todos', (request, response) => {
 	});
 });
 
+app.get('/todos', (request, response) => {
+	Todo.find().then((todos) => {
+		response.send({todos});
+	}, (error) => {
+		response.status(400).send(error);
+	});
+});
+
 app.listen(3000, () => {
 	console.log('Servidor iniciado en el puerto 3000');
 });
