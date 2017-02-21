53d0f1e7adc21c6341ae8278937c86938985f40d
Diego <diego.paniagua.kanon89@gmail.com>
2017-02-20 18:55:28 -0600

    Agregando pruebas unitarias para peticion GET /todos/:id

--- server/tests/server-test.js
+++ server/tests/server-test.js
@@ -1,12 +1,15 @@
 const expect = require('expect');
 const request = require('supertest');
+const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
 const todos = [{
+	_id: new ObjectID(),
 	text: 'Primer prueba todo',
 }, {
+	_id: new ObjectID(),
 	text: 'Segunda prueba todo'
 }];
 
@@ -68,4 +71,32 @@ beforeEach((done) => {
 				expect(response.body.todos.length).toBe(2);
 			}).end(done);
 		});
-	})
+	});
+
+	describe('GET /todos/:id', () => {
+		it('Debería regresar todo el documento', (done) => {
+			request(app).get(`/todos/${todos[0]._id.toHexString()}`)
+			.expect(200).expect((response) => {
+				expect(response.body.todo.text).toBe(todos[0].text);
+			})
+			.end(done);
+		});
+
+		it('Debería de regresar un 404 si no encuentra registros', (done) => {
+			var hexId = new ObjectID().toHexString();
+
+			request(app)
+			.get(`/todos/${hexId}`)
+			.expect(404)
+			.end(done);
+		});
+
+		/*
+		it('Debería de regresar un 404 para los objetos no encontrados con relacion al id', (done) => {
+			request(app)
+			.get('/todos/123abc')
+			.expect(404)
+			.end(done);
+		});
+		*/
+	});
