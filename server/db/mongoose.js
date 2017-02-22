e978fe275ac94298341f5c0b8f9609cccb289799
Diego <diego.paniagua.kanon89@gmail.com>
2017-02-22 12:31:38 -0600

    Configurando por separado las pruebas y ambientes de desarrollo

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,8 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp');
+//mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp'); //Para desarrollo
+mongoose.connect(process.env.MONGODB_URI); //Para produccion
 
 module.exports = {
 	mongoose: mongoose
